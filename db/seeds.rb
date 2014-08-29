e6a68f329f710d67606663d6e38ec29386a63e02
vzmind <vzmind@gmail.com>
2014-08-29 17:18:57 +0100

    create initial DB/SEED file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'admin@edes.io', password: 'passpass', password_confirmation: 'passpass', role: 'superadmin')
+o = Organization.create!(name: 'Itsu')
+Location.create!(name: 'Itsu Regent street', address: '21 regent street', city: 'London', zipcode: 'EC1234', landline: '0123456789', organization_id: o.id)
